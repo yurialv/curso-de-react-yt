f720954c13dc694e95a0fe9d5486c57e8dd57660
Yuri Alves <yuribarbearia@icloud.com>
2025-04-22 16:51:45 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
+import Title from "./components/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title: title,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <Title>Gerenciador de Tarefa</Title>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Input from "./Input";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <Input
+        type="text"
+        placeholder="Digite o título da Tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert("Preenche o titulo e a descrição da tarefa.");
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;

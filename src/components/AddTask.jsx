3115610e5bc264e13722c8223c89a9757836dd6c
Yuri Alves <yuribarbearia@icloud.com>
2025-04-22 18:12:51 -0300

    Melhora visual na lista de tarefas com ícone de concluído 2

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,33 +1,46 @@
+import { Input } from "postcss";
 import { useState } from "react";
-import Input from "./Input";
 
 function AddTask({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+
+  function handleSubmit() {
+    if (!title.trim() || !description.trim()) {
+      return alert("Preencha o título e a descrição da tarefa.");
+    }
+    onAddTaskSubmit(title, description);
+    setTitle("");
+    setDescription("");
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      e.preventDefault(); // impede o comportamento padrão
+      handleSubmit();
+    }
+  }
 
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <Input
         type="text"
         placeholder="Digite o título da Tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <Input
         type="text"
         placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <button
-        onClick={() => {
-          if (!title.trim() || !description.trim()) {
-            return alert("Preenche o titulo e a descrição da tarefa.");
-          }
-          onAddTaskSubmit(title, description);
-          setTitle("");
-          setDescription("");
-        }}
+        onClick={handleSubmit}
         className="bg-slate-500 text-white px-4 py-2 rounded-md"
       >
         Adicionar
